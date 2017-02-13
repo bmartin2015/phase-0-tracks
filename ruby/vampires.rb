90e085bac2338b62d5a77e3564509db2a472d097
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-13 17:19:01 -0600

    Update vampires.rb with Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,37 @@ garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company’s health insurance?"
 insurance = gets.chomp
+
+vampire = false
+
+age_calc = 2017 - birth_year.to_i
+
+if age_calc != age.to_i
+	age_correct = false
+else
+	age_correct = true
+end
+
+if garlic_bread == "yes" || garlic_bread == "Yes"
+	garlic = true
+else
+	garlic = false
+end
+
+if insurance == "yes" || insurance == "Yes"
+	ins = true
+else
+	ins = false
+end
+
+if name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire."
+elsif !age_correct && !garlic && ins
+	puts "Almost certainly a vampire"
+elsif !age_correct && (!garlic || !ins)
+	puts "Probably a vampire."
+elsif age_correct && garlic && ins
+	puts "Probably not a vampire."
+else
+	puts "Results inconclusive."
+end
