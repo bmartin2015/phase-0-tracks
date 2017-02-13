8a3c0a9158ec042f320e14352b24f19a3c164ed4
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-13 17:55:18 -0600

    Update vampires.rb with some comments and stuff

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,13 @@
+# vampires.rb 
+# DBC 4.4 Solo Challenge
+
+# Ask how many employees will be processed in each pass
 puts "How many employees will be processed?"
 emp_count = gets.chomp.to_i
+x = 0
 
-until emp_count == 0
+# Loop the code until all employees are processed.
+until x >= emp_count
 
 	puts "What is your name?"
 	name = gets.chomp 
@@ -9,7 +15,7 @@ until emp_count == 0
 	puts "How old are you?"
 	age = gets.chomp
 
-	puts "What year where your born?"
+	puts "What year were your born?"
 	birth_year = gets.chomp
 
 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
@@ -17,6 +23,7 @@ until emp_count == 0
 
 	puts "Would you like to enroll in the company’s health insurance?"
 	insurance = gets.chomp
+
 
 	age_calc = 2017 - birth_year.to_i
 
@@ -26,6 +33,7 @@ until emp_count == 0
 		age_correct = true
 	end
 
+	# initialize a blank string for vampire
 	vampire = String.new
 
 	if garlic_bread == "yes" || garlic_bread == "Yes"
@@ -40,26 +48,32 @@ until emp_count == 0
 		ins = false
 	end
 
+	# if the user got all impute correct, they are probably not a vampire
 	if age_correct && garlic && ins
 		vampire = "Probably not a vampire."
 	end
 
+	# if the user did not get their age correct, and also didn't like garlic or insurance, they might be a vampire
 	if !age_correct && (!garlic || !ins)
 		vampire = "Probably a vampire."
 	end
 
+	# if user got all things wrong, they are a vampire
 	if !age_correct && !garlic && !ins
 		vampire = "Almost certainly a vampire"
 	end
 
+	# cross check for known vampires
 	if name == "Drake Cula" || name == "Tu Fang"
 		vampire = "Definitely a vampire."
 	end
 
+	# if none of the above are met, we just don't know
 	if vampire == ""
 		vampire = "Results inconclusive."
 	end
 
+	# check for allergies
 	allergy = String.new
 	puts "Please input allergies, one at time. Type done when finished."
 	until allergy == "done"
