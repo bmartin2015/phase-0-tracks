9bdbe4510c00d129bc11b10500a05ac72996fae8
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-14 16:00:55 -0600

    Update hamsters.rb with pair code for Release 2 and 3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,19 +2,30 @@ puts "What is the hamsters name?"
 hamster_name = gets.chomp
 
 puts "What is the volume level? (1-10)"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 
 puts "What is the fur color?"
 fur_color = gets.chomp
 
-puts "Is the hamster a candidate for adoption?"
+puts "Is the hamster a candidate for adoption? (y/n)"
 adoption = gets.chomp
+if adoption == "yes" || adoption == "y"
+	adoption = true
+else
+	adoption = false
+end
 
 puts "What is the estimated age?"
-estimated_age = gets.chomp
+estimated_age = gets.chomp.to_i
 
-if estimated_age == ""
+
+if estimated_age == 0
 	estimated_age = nil
-else
-	estimated_age = estimated_age
 end
+
+
+puts "Hamster name: #{hamster_name}"
+puts "Volume level: #{volume_level}"
+puts "Fur color: #{fur_color}"
+puts "Adoption desirability: #{adoption}"
+puts "Estimated age: #{estimated_age}"
