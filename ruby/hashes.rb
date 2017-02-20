641f784f14bf919ddbaa93f720d2281b1bbb7ca0
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-20 10:44:14 -0600

    Add pseudocode to hashes.rb

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+# PSEUDOCODE
+# -Initialize hash
+# -Ask user for each of the following and add to hash
+#  -Name (string)
+#  -Age (int)
+#  -number of children (int)
+#  -decor theme (string)
+#  -number of rooms (int)
+#  -wheelchair accessible (bool)
+#  	-Convert to bool
+#  -pets (bool)
+#  	-Convert to bool
+#  -three favorite colors (array of strings)
+#  	-Convert to array
+# -Print out hash in user readable format
+# -Prompt user to enter in key name for any errors, 'none' if none
+#  -Until none
+#   -ask for key name
+#   -convert key name to symbol
+#   -ask for updated information and store in key
+# -Print hash and exit
