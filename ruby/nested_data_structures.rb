8b81c7c3db88484e6e810b940a37690bcea10204
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-23 17:08:15 -0600

    Create data structure for Orient Express

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+train = {
+	"Sleeper Car One": {
+		car_name: "Constantinople - Calais",
+		first_class_berths: 8,
+		second_class_berths: 4,
+		first_class_passengers: ["Poirot","Ratchett","Hubbard","H Andrenyi", "R. Andrenyi", "Dragomiroff", "Arbthnot", "Hardman"],
+		second_class_passengers: ["Masterman", "Foscarelli", "Schmidt", "MacQueen", "Ohisson", "Debenham"]
+		},
+	"Sleeper Car Two": {
+		car_name: "Athens - Paris",
+		first_class_berths: 8,
+		second_class_berths: 4,
+		first_class_passengers: ["Constantine", "Bouc"],
+		second_class_passengers: []
+		},
+	"Dining Car": {
+			beverages: ["perrier", "hot coffee", "orange juice", "liqueur", "cup of tea", "cognac"],
+			meals: ["eggs (including omelet", "soup", "delicate cream cheese", "chicken (without sauces)", "boiled fish", "cereal", "biscuits"]
+		},
+	"Coach One": ["Not accessible"]
+}
+
+print train
