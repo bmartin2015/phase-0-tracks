d41940edc0cd46a561e6895f4183633d4b0f8aac
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-23 15:37:52 -0600

    Add fib method to problem_solving_methods.rb

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,5 @@
+# Release 0
+
 arr = [5,34,256]
 
 def search_array(arr, serach_number)
@@ -13,3 +15,19 @@ def search_array(arr, serach_number)
 end
 
 p search_array(arr, 100)
+
+# Release 1
+# 2: [0,1] 
+# 3: [0,1,1] = 0 + 1
+# 6: [0,1,1,2,3,5] = 2 + 3
+# 7: [0,1,1,2,3,5,8] = 3 + 5
+
+def fib(number)
+	fib_list = [0,1]
+	until fib_list.length == number
+		fib_list << fib_list[-1] + fib_list[-2]
+	end
+	return fib_list[-1]
+end
+
+print fib(100) == 218922995834555169026 
