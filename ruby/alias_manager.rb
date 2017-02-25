07bef65a91b9ad782e636ebfe264781ca5be28e9
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-25 09:03:40 -0600

    Refactor code to use map and make more simple

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -27,44 +27,42 @@
 # 		Test against the range ('A'..'Z')
 # 	other edge cases? Such as an apostrophe
 
+# Notes - Reworked after completing pairing session with .map!
 
-# I would like to refactor these down, they seem really big
-
-# Return the next vowel for letter
-def next_vowel(letter)
-	vowels = ["a", "e", "i", "o", "u"]
-	uppercase = is_upcase?(letter) # Test if letter is uppercase and remember
-	letter = letter.downcase # make the letter lower case if it is not
-	# If the vowel is u, need to set it to to a
-	if vowels.index(letter) != (vowels.length - 1)
-		next_vowel = vowels[vowels.index(letter)+1]
-	else
-		next_vowel = vowels[0]
-	end
-	# If the letter started upper case, return it as upper case, otherwise it's lower case
-	if uppercase
-		next_vowel.upcase
-	else
-		next_vowel
-	end
+# Swap the first and last name, will leave middles names in the middle
+def name_swap(name)
+	split_name = name.split(" ") # Split that name at spaces
+	# If the user has more than two names, it will leave them in the middle
+	split_name[0], split_name[-1] = split_name[-1], split_name[0]
+	return split_name
 end
 
-# return the next consonant - pretty much the same as next_vowel()
-def next_consonant(letter)
-	consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-	uppercase = is_upcase?(letter)
-	letter = letter.downcase
-	if consonants.include?(letter)
-		if consonants.index(letter) != (consonants.length - 1)
-			next_consonant = consonants[consonants.index(letter)+1]
+# Return the next letter based on if vowel or not
+def next_letter(letter)
+	uppercase = is_upcase?(letter) # Test if letter is uppercase and remember
+	letter = letter.downcase # make the letter lower case if it is not
+	if "aeiou".include?(letter) # checks to see if a letter is a vowel
+		vowels = ["a", "e", "i", "o", "u"]
+		if vowels.index(letter) != (vowels.length - 1) 
+			new_letter = vowels[vowels.index(letter)+1]
 		else
-			next_consonant = consonants[0]
+			new_letter = vowels[0] # Change 'u' to 'a'
+		end
+	else
+		consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+		if consonants.include?(letter)
+			if consonants.index(letter) != (consonants.length - 1)
+				new_letter = consonants[consonants.index(letter)+1]
+			else
+				new_letter = consonants[0] # Change 'z' to 'b'
+			end
 		end
 	end
-	if uppercase
-		next_consonant.upcase
+	# if the letter was upper case, upcase it
+	if uppercase 
+		new_letter.upcase
 	else
-		next_consonant
+		new_letter
 	end
 end
 
@@ -73,61 +71,41 @@ def is_upcase?(letter)
 	('A'..'Z').include?(letter)
 end
 
-# checks to see if a letter is a vowel
-def is_vowel?(letter)
-	"aeiou".include?(letter.downcase)
-end
-
-# Swap the first and last name, will leave middles names in the middle
-def name_swap(name)
-	# Split that name at spaces
-	split_name = name.split(" ")
-
-	# If the user has more than two names, it will leave them in the middle
-	split_name[0], split_name[-1] = split_name[-1], split_name[0]
-	return split_name
-end
-
 # This will run the methods needs to create one alias
 def alias_generator(real_name)
 	alias_name = []	# Blank array to hold the final names
 	swapped_names = name_swap(real_name) # Swap the first and last name
+
 	swapped_names.each do |name| # for each array index, change the letters
 		letters = name.split("")
 		alias_letter = []
-		letters.each do |letter|
-			if is_vowel?(letter) # is the letter a vowel?
-				next_letter = next_vowel(letter)
+
+		letters.map! do |letter| # for each letter in letters, change to the next letter
+			new_letter = next_letter(letter)
+			if new_letter == nil # if the value wasn't a letter (such as "'"), it will return nil
+				""
 			else
-				next_letter = next_consonant(letter)
-			end
-			if next_letter == nil
-				alias_letter << ""
-			else
-				alias_letter << next_letter
+				new_letter
 			end
 		end
-		alias_name << alias_letter.join
+		alias_name << letters.join
 	end
 	alias_name.join(" ")
 end
 
+
 # this is the UI for the program
-def alias_manager()
-	agent_names = {}
-	user_input = ""
-	until user_input == "done" || user_input == "Done"
-		puts "Please enter agent name or type 'done' to exit:"
-		user_input = gets.chomp
-		if user_input != "done"
-			alias_name = alias_generator(user_input)
-			puts "#{user_input} is #{alias_name}."
-			agent_names[user_input] = alias_name
-		end
-	end
-	agent_names.each do |agent_alias|
-		puts "#{agent_alias[1]} is actually #{agent_alias[0]}."
+agent_names = {}
+user_input = ""
+until user_input == "done" || user_input == "Done"
+	puts "Please enter agent name or type 'done' to exit:"
+	user_input = gets.chomp
+	if user_input != "done"
+		alias_name = alias_generator(user_input)
+		puts "#{user_input} is #{alias_name}."
+		agent_names[user_input] = alias_name
 	end
 end
-
-alias_manager() # run the program
+agent_names.each do |real_name, alias_name|
+	puts "#{alias_name} is actually #{real_name}."
+end
