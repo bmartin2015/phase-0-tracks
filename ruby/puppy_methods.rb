4902f59daeecdba1d8d4b02395563f9120ef5676
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-28 15:46:38 -0600

    Update with Release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,4 @@
 class Puppy
-
   def fetch(toy)
   	@toy = toy
     puts "I brought back the #{toy}!"
@@ -25,7 +24,33 @@ class Puppy
   def initialize()
   	puts "Initializing new puppy instance."
   end
+end
 
+# Release 2
+class Cat
+	def initialize()
+		puts "Initializing new cat instance."
+	end
+
+	def cats_speech(meow)
+		puts "The cat makes a #{meow} sound."
+	end
+
+	def move(dir)
+		puts "The cat #{dir} off the couch."
+	end
+end
+
+cat_array = []
+
+50.times do |x|
+	x = Cat.new
+	cat_array << x
+end
+
+cat_array.each do |cat_name|
+	cat_name.cats_speech("meow")
+	cat_name.move("jumps")
 end
 
 # Driver code
@@ -36,4 +61,3 @@ spot.speak(3)
 spot.rollover()
 puts spot.dog_years(7)
 spot.treat("bone")
-
