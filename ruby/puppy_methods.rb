f7f2d48b1e8e078ed5af4d0bbd94776aa1b92740
Ben Martin <benjamin.martin2010@gmail.com>
2017-02-28 15:30:39 -0600

    Update code for Release 0 and Release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,39 @@
 class Puppy
 
   def fetch(toy)
+  	@toy = toy
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(number)
+  	number.times { puts "Woof!" }
+  end
+
+  def rollover()
+  	puts "rolls over"
+  end
+
+  def dog_years(age)
+  	return age.to_i * 7
+  end
+
+  def treat(food)
+  	puts "eats the #{food}"
+  end
+
+  def initialize()
+  	puts "Initializing new puppy instance."
+  end
+
 end
+
+# Driver code
+spot = Puppy.new()
+
+spot.fetch("ball")
+spot.speak(3)
+spot.rollover()
+puts spot.dog_years(7)
+spot.treat("bone")
 
