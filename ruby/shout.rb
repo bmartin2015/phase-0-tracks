54ac8790addeb6011feabc5a8722ebf94cff5aea
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-02 07:28:10 -0600

    Create Shout mixin and classes to use it

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,39 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-	def self.yell_angrily(words)
+	# Release 1 code
+#	def self.yell_angrily(words)
+#		words + "!!!" + " :("
+#	end
+#
+#	def self.yell_happily(words)
+#		words.upcase + "!!!" + " :D"
+#	end
+
+	# Release 3 code
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words.upcase + "!!!" + " :D"
 	end
 end
 
-# Driver Code
-puts Shout.yell_angrily("stop")
-puts Shout.yell_happily("yay")
+# Classes - Release 2
+class Prairie_dog
+	include Shout
+end
+
+class Customer
+	include Shout
+end
+
+# Driver code - Release 2
+prairie_dog = Prairie_dog.new
+customer = Customer.new
+
+puts prairie_dog.yell_happily("bark")
+puts customer.yell_angrily("I want to speak to your manager")
+
+# Driver Code - Release 1
+# puts Shout.yell_angrily("stop")
+# puts Shout.yell_happily("yay")
