e84f6e33681598da9919f860b904befb37202b7f
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-03 16:51:47 -0600

    Refactor code to make a bit easier to understand

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -12,27 +12,30 @@
 # 	if guess is duplicate
 # 		let user know it was already guessed, but don't deduct count
 # 	If guess is wrong
-# 		let user know it was wrong and deduct 1 from guess count
+# 		deduct 1 from guess count
 
 # If the user won - print congratulatory message
 # If the user lost - print taunting message
 
-# initialize method to setup new class
-# input: string
-# steps:
-	# Convert string to array
-	# Create second array with "_" for each letter in string
-	# Calculate total number of guesses allowed
-# output: instance of WordGame
-class WordGame
-	attr_reader :word, :word_guess, :guess_count, :words_guessed
 
+# class WordGame
+class WordGame
+	attr_reader :guess_count, :word_guess; :words_guessed; :word; :word_array
+
+	# initialize method to setup new class
+	# input: string
+	# steps:
+		# Convert string to array
+		# Create second array with "_" for each letter in string
+		# Create empty words_guessed array
+		# Calculate total number of guesses allowed
+	# output: instance of WordGame
 	def initialize(word)
 		@word = word
 		@word_array = word.split("")
 		@word_guess = Array.new(@word.length, "_")
 		@words_guessed = []
-		@guess_count = @word.length * 1
+		@guess_count = @word.length * 5
 	end
 
 	# Print out feedback from guess array ("_ _ _ _ _") using "_" for unknown letters
@@ -42,9 +45,7 @@ class WordGame
 	# output: string
 	def print_word
 		word_string = ""
-		word_guess.each do |letter|
-			word_string = word_string + letter + " "
-		end
+		word_guess.each { |letter| word_string = word_string + letter + " " }
 		return word_string.rstrip
 	end
 
@@ -63,17 +64,19 @@ class WordGame
 	# output: bool
 	def guess_word(guess)
 		guess_array = guess.split("")
+
+		# No penalty for duplicate words
+		return false if @words_guessed.index(guess) != nil
+
 		if guess == @word
 			return true
-		elsif @words_guessed.index(guess) != nil
-			return false
 		else
 			guess_array.length.times do |index|
 				if guess_array[index] == @word_array[index]
 					@word_guess[index] = guess_array[index]
 				end
 			end
-			words_guessed << guess
+			@words_guessed << guess
 			@guess_count -= 1
 			return false
 		end
@@ -85,7 +88,7 @@ class WordGame
 		# make array of guessed words into string separated by commas
 	# output: string
 	def print_words_guessed
-		return "Words Guessed: " + words_guessed.join(", ")
+		return "Words Guessed: " + @words_guessed.join(", ")
 	end
 end
 
@@ -96,7 +99,6 @@ puts "What word would you like the other player to guess?"
 word_to_guess = gets.chomp
 
 # Loop until out of guesses or answer is correct
-
 game = WordGame.new(word_to_guess)
 game_over = false
 
@@ -109,8 +111,9 @@ until game_over == true || game.guess_count == 0
 	game_over = game.guess_word(guess)
 end
 
+# If the user won - print congratulatory message
 if game_over == true
 	puts "You Won! YAYA!"
-else
+else # If the user lost - print taunting message
 	puts "HAHA, you lost!"
 end
