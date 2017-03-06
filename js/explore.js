36a076a863e1ba25546ebd45a91ee89bb6c249d0
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-06 12:08:55 -0600

    Create explore.js and pseudocode reverse function

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Function that takes a string and reverses it
+// input: STRING
+// steps:
+//	FOR EACH letter in STRING counting down
+//	  take letter and add to output string
+// output: string
