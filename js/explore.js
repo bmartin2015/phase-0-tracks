9e54b798f593b3963ac06c55f0218affd34d59a5
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-06 12:13:40 -0600

    Create reverse function to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,13 @@
 //	FOR EACH letter in STRING counting down
 //	  take letter and add to output string
 // output: string
+function reverse(input_string) {
+	var output_string = '';
+	for(i = input_string.length-1; i >= 0; i--) {
+		output_string += input_string[i];
+	}
+	return output_string
+}
+
+// TEST CODE
+console.log(reverse("Hello"));
