2a8258e31b23fdbc0945338c812807f7b1015d52
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-08 17:05:43 -0600

    Add randomWords function to generate random strings

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+// RELEASE 0
 // In algos.js, write a function that takes an array of words or phrases and returns the longest word or phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason through this based on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. Note that "some string".length will do what you expect in JS.
 // Add driver code that tests your function on a few arrays.
 
@@ -21,7 +22,8 @@ function longestString(arr) {
 	return arr[stored_index];
 };
 
-// Function that takes two objects and cmapirs if they share at least one key-value pair
+// RELEASE 1
+// Function that takes two objects and checks if they share at least one key-value pair
 // Input: two objects
 // Steps:
 	// FOR each key in object 1
@@ -43,6 +45,33 @@ function twoObjects(obj1, obj2) {
 	return match;
 };
 
+//RELEASE 2
+// Function that takes an integer and returns an array of strings of the given length
+// Input: integer
+// Steps:
+	// loop the number of times for the given integer
+		// Generate a random number for length of word
+		// loop the number of letters
+			// Generate a random letter and add to word
+		// add word to array
+		// return array
+// Output: array
+function randomWords(numberWords) {
+	var wordList = [];
+	alphabet = "abcdefghijklmnopqrstuvwyz"
+	for(i = 1; i <= numberWords; i++) {
+		word = "";
+		rand = Math.floor(Math.random() * 10) + 1;
+		for (x = 0; x < rand; x++ ) {
+			word += alphabet[Math.floor(Math.random() * alphabet.length)];
+		}
+		wordList.push(word);
+	}
+	return wordList;
+}
+
+
+
 // TEST CODE - RELEASE 0
 // var str = "";
 // var arr = ["long phrase","longest phrase","longer phrase"];
@@ -56,12 +85,15 @@ function twoObjects(obj1, obj2) {
 
 
 // TEST CODE - RELEASE 1
-var obj1 = {name: "Steven", age: 54};
-var obj2 = {name: "Tamir", age:54};
+// var obj1 = {name: "Steven", age: 54};
+// var obj2 = {name: "Tamir", age:54};
 
-console.log(twoObjects(obj1, obj2));
+// console.log(twoObjects(obj1, obj2));
 
-var newObj1 = {name: "Steven", age: 22};
-var newObj2 = {name: "Allen", age: 38, height: "5'6"}
+// var newObj1 = {name: "Steven", age: 22};
+// var newObj2 = {name: "Allen", age: 38, height: "5'6"}
 
-console.log(twoObjects(newObj1, newObj2));
+// console.log(twoObjects(newObj1, newObj2));
+
+test = randomWords(10)
+console.log(test)
