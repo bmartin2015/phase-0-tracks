3afe9d91a1b23c60a6264d999f0910d723b4ef4e
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-08 16:26:39 -0600

    Add function twoObjects to compair if two objects share any key-value pairs

--- js/algos.js
+++ js/algos.js
@@ -13,20 +13,55 @@
 function longestString(arr) {
 	var stored_index = 0; // Set default index
 	for(i = 0; i < arr.length; i++) {
+		// I recall from my University JS class that you can address strings like arrays
 		if(arr[stored_index].length <= arr[i].length) {
 			stored_index = i;
-		}
-	}
+		};
+	};
 	return arr[stored_index];
-}
+};
 
-// TEST CODE
-var str = "";
-var arr = ["long phrase","longest phrase","longer phrase"];
-console.log(longestString(arr));
+// Function that takes two objects and cmapirs if they share at least one key-value pair
+// Input: two objects
+// Steps:
+	// FOR each key in object 1
+		// IF current key-value is equal to any key-value in object 2
+			// set match = TRUE
+		// ELSE
+			// match = FALSE
+	// RETURN match
+// Output: bool
+function twoObjects(obj1, obj2) {
+	match = false;
+	for (var key in obj1) {
+		if (obj1[key] == obj2[key]) {
+			match = true;
+		} else {
+			match = false
+		};
+	};
+	return match;
+};
 
-var test = ["123456790","12345","2538","1234578"];
-console.log(longestString(test));
+// TEST CODE - RELEASE 0
+// var str = "";
+// var arr = ["long phrase","longest phrase","longer phrase"];
+// console.log(longestString(arr));
 
-var test2 = ["12345", "123456", "12345678", "111111111111111111111"];
-console.log(longestString(test2))
+// var test = ["123456790","12345","2538","1234578"];
+// console.log(longestString(test));
+
+// var test2 = ["12345", "123456", "12345678", "111111111111111111111"];
+// console.log(longestString(test2))
+
+
+// TEST CODE - RELEASE 1
+var obj1 = {name: "Steven", age: 54};
+var obj2 = {name: "Tamir", age:54};
+
+console.log(twoObjects(obj1, obj2));
+
+var newObj1 = {name: "Steven", age: 22};
+var newObj2 = {name: "Allen", age: 38, height: "5'6"}
+
+console.log(twoObjects(newObj1, newObj2));
