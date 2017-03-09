1c8e6593bca9acf00bb830aaefdddea43d5501d2
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-08 19:29:26 -0600

    Update with my comments

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge Kelly Somerville.
+# We spent 1 hour 20 minutes on this challenge.
 
 # EXPLANATION OF require_relative
 # It allows the data in the state_data.rb file to be accessible in my_soultion.
@@ -40,6 +40,7 @@ class VirusPredictor
       percent_dead = 0.05
     end
 
+    # only do the number of deaths calculate at the end.
     number_of_deaths = (@population * percent_dead).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
@@ -53,16 +54,19 @@ class VirusPredictor
     speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      rate_of_spread = 0.5
     elsif @population_density >= 150
-      speed += 1
+      rate_of_spread = 1
     elsif @population_density >= 100
-      speed += 1.5
+      rate_of_spread = 1.5
     elsif @population_density >= 50
-      speed += 2
+      rate_of_spread = 2
     else
-      speed += 2.5
+      rate_of_spread = 2.5
     end
+
+    # only do the += calculation at the end
+    speed += rate_of_spread
 
     puts " and will spread across the state in #{speed} months.\n\n"
 
@@ -98,3 +102,14 @@ end
 
 #=======================================================================
 # Reflection Section
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# The first is the hash rocket, with it you can use a string as the key. The second method sets the key as a symbol.
+# What does require_relative do? How is it different from require?
+# require_relative will bring in a file and will start by looking in the local directory. require will start at the top of the structure and work its way down. require can also bring in outside modules. 
+# What are some ways to iterate through a hash?
+# You can iterate through a has via .each, or each_pair.
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# All instances variables are accessible across the class, so no need to pass them - I didn't see this at first and had to be talked through it.
+# What concept did you most solidify in this challenge?
+# How instance variables work became much more clear. I also learned about he private and public keywords.
+#=======================================================================
