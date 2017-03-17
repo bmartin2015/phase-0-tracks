2a64d74a9e4a39e475aff8529e9323c8f394fd51
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-17 16:46:27 -0500

    Add list_games method to output list of games

--- databases/bg_tracker/menu.rb
+++ databases/bg_tracker/menu.rb
@@ -17,6 +17,14 @@ class Menu
 headtext
 	end
 
+# make a list of choices and ask the user to pick one
+# input: array of menu choices
+# steps:
+	# LOOP thru menu choices
+		# Print menu item with a number
+	# GET user input 
+	# RETRUN text of the users choice
+# output: array
 	def menu_options(menu_array)
 		counter = 1
 		menu_array.each do |menu_item|
@@ -26,5 +34,35 @@ headtext
 		puts "Please select an option from above."
 		choice = gets.chomp.to_i
 		menu_array[choice-1]
+	end
+
+# List boardgames in a nice format
+# input: hash of boardgames, array of headers
+# steps:
+	# 
+# output: string of boardgames
+	def list_games(boardgame_hash, header_array)
+		output = ""
+		header_lengths = {}
+		header_array.each do |head|
+			header_lengths[head] = 0
+		end
+		boardgame_hash.each do |game, game_info|
+			header_lengths.each do |head, length|
+				header_lengths[head] = game_info[head].length if game_info[head].length > length
+			end
+		end
+		width = 15
+		header_lengths.each {|x,y| width += y}
+		output += "-" * (width+4) + "\n|"
+		header_array.each {|head| output += head.center(header_lengths[head]+5) + "|"}
+		output += "\n" + "-" * (width+4) + "\n|"
+		boardgame_hash.each do |game, game_info|
+			header_lengths.each do |name, length|
+				output += game_info[name].ljust(length+5) + "|"
+			end
+			output+= "\n|"
+		end
+		output += "-" * (width+2) + "|"
 	end
 end
