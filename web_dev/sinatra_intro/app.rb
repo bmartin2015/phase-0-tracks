23aed81f319555c883966cd498926ca3d3610e75
Ben Martin <benjamin.martin2010@gmail.com>
2017-03-21 08:45:45 -0500

    Add methods for Release 0

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -43,4 +44,30 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+
+# Release 0
+# 1. A /contact route that displays an address (you can make up the address).
+get '/contact' do
+	address = "123 Main St.<br>New Canton, Zombieland 00005"
+	address
+end
+
+# 2. A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+get '/great_job' do
+	name = params[:name]
+	if name
+		"Goodo job, #{name}"
+	else
+		"Good job!"
+	end
+end
+
+# 3. A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/add_numbers' do
+	number_1 = params[:number_1].to_i
+	number_2 = params[:number_2].to_i
+	number_3 = number_1 + number_2
+	number_3.to_s
 end
